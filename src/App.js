ad1ce0e8c338cf2f13cce9d53e344e8b791aca16
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-06 17:06:30 -0700

    Add(App): added objects to app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import './App.css';
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList/NotesList';
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: 'This is 1 note',
+      date: '13/04/2001',
+    },
+    {
+      id: nanoid(),
+      text: 'This is 2 note',
+      date: '13/04/2001',
+    },
+    {
+      id: nanoid(),
+      text: 'This is 3 note',
+      date: '13/04/2001',
+    },
+  ]);
+
   return (
-    <div className='App'>
-      <div></div>
+    <div className='container'>
+      <NotesList notes={notes} />
     </div>
   );
 }
