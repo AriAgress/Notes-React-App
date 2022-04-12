1b5a5661904f26c155a2dd6c23ee9e46af79af7d
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-12 14:38:38 -0700

    Add(Header): created header component

---
+++ src/components/Header/header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const header = () => {
+  return (
+    <div className='header'>
+      <h1>Notes</h1>
+      <button className='save'>Toggle Mode</button>
+    </div>
+  );
+};
+
+export default header;
