c838922584641e9cdfa34214dc738d607e6c07ad
Ariel Agress <ari@ariels-mac-mini.attlocal.net>
2022-04-12 14:22:13 -0700

    Add(App): Search bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import NotesList from './components/NotesList/NotesList';
+import Search from './components/Search/Search';
 
 function App() {
   const [notes, setNotes] = useState([
@@ -39,6 +40,7 @@ function App() {
 
   return (
     <div className='container'>
+      <Search />
       <NotesList
         notes={notes}
         handleAddNote={addNote}
